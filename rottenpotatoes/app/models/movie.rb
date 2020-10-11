6a124ed61b22e10cc52edc02db2df36dff719460
EC2 Default User <ec2-user@ip-172-31-35-31.us-east-2.compute.internal>
2020-10-11 00:26:19 +0000

    first commit

--- rottenpotatoes/app/models/movie.rb
+++ rottenpotatoes/app/models/movie.rb
@@ -3,3 +3,9 @@ class Movie < ActiveRecord::Base
     %w(G PG PG-13 NC-17 R)
   end
 end
+class Movie_Edit < ActiveRecord::Migration 
+  #def self.add_column
+    add_column(:movies, :director, :string, limit: 20)
+ # end
+end
+
